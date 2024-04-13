9f78e3cd5841bd9e13f5760926ea6c52fb81b345
Juan Rodriguez <jua0729.jr@gmail.com>
2024-04-13 11:41:19 -0500

    added User model, there already migrated, and setup AUTH_USER in settings.py

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,3 +1,19 @@
+# Django
 from django.contrib import admin
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
 
-# Register your models here.
+from simple_history.admin import SimpleHistoryAdmin
+from apps.user.models import *
+
+
+
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ('id','username','name','last_name','document','phone_number')
+    list_display = ('id','username','name','last_name','document','phone_number','is_active')
+    list_filter = ['is_active']
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Permission)
+admin.site.register(ContentType)
--- apps/user/models.py
+++ apps/user/models.py
@@ -1,3 +1,99 @@
-from django.db import models
+# stantard library
+import uuid
 
-# Create your models here.
+# Django
+from django.db import models
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.forms import model_to_dict
+
+from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
+
+
+# simple_history
+from simple_history.models import HistoricalRecords
+
+class UserManager(BaseUserManager):
+    def _create_user(self, 
+                     username, 
+                     name,
+                     last_name,
+                     password,
+                     is_staff,
+                     is_superuser,
+                     **extra_fields):
+        user = self.model(
+            username = username,
+            name = name,
+            last_name = last_name,
+            is_staff = is_staff,
+            is_superuser = is_superuser,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_user(self, username, name,last_name, password=None, **extra_fields):
+        return self._create_user(username, 
+                                 name,
+                                 last_name,
+                                 password,
+                                 False,
+                                 False, 
+                                 **extra_fields)
+
+    def create_superuser(self, username, name,last_name, password=None, **extra_fields):
+        return self._create_user(username, 
+                                 name,
+                                 last_name,
+                                 password,
+                                 True,
+                                 True,
+                                 **extra_fields)
+    
+# Usuario model
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField( max_length = 250,
+                                 unique = True)
+    name = models.CharField('Names', 
+                            max_length = 255, 
+                            blank = True, 
+                            null = True)
+    token = models.UUIDField('Token',
+                             primary_key = False,
+                             editable = False, 
+                             null = True, 
+                             blank = True)
+    last_name = models.CharField('Last names', 
+                                 max_length = 255,
+                                 blank = True, 
+                                 null = True)
+    document = models.PositiveBigIntegerField('Document', 
+                                              unique=True, 
+                                              blank=True, 
+                                              null = True,)
+    phone_number = models.PositiveBigIntegerField('Phone number',  
+                                                  blank=True,
+                                                  null = True)
+    is_active = models.BooleanField('State', 
+                                    default = False)
+    is_staff = models.BooleanField(default = False)
+
+    historical = HistoricalRecords()
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def toJSON(self):
+        item = model_to_dict(self)
+        return item
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['name','last_name']
+
+    def _str_(self):
+        return f'{self.name} {self.last_name}'
