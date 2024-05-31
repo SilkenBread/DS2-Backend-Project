7bf9669d825a4cfddc4e1f15b7334b9a8c7afa69
Juan Rodriguez <jua0729.jr@gmail.com>
2024-05-31 16:36:39 -0500

    added port in allowed cors

--- config/settings.py
+++ config/settings.py
@@ -100,7 +100,8 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+    "http://127.0.0.1:5173",
 ]
 
 CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000']
